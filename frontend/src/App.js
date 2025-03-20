89ff68e52b4f0bb85b39e96a42252f8a0463d54a
Anna Ricker <anna.ricker8@gmail.com>
2025-03-20 16:15:25 +0100

    commit also frontend stuff of work in progress and activated checheckstyle workflow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import GenderChoice from "./ChoiceBox/GenderChoice";
+
 
 function App() {
   const [recommendation, setRecommendation] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:3050/recommendation')
+  const [selectedGender, setSelectedGender] = useState("");
+  
+  const handleClick = () => {
+    fetch('http://localhost:3050/recommendation?input=${selectedGender}')
       .then(response => response.json())
       .then(data => setRecommendation(data.recommendation))
       .catch(error => console.log(error));
-  }, []);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Recommendation</h1>
+      <div class="App-header">Recommender System</div>
+      <div class= "ChoiceSelection">
+        <GenderChoice onGenderSelect={setSelectedGender} /> 
+      </div>
+      <button
+          onClick={handleClick}
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+        >
+          Recommend
+      </button>
+      <div class="Recommendations">
+        <h1>Recommendations</h1>
         <p>{recommendation}</p>
-      </header>
+      </div>
     </div>
   );
 }

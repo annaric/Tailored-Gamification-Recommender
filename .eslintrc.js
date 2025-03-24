6e5536aad69f83324befd0225e9ecc488a51303c
annaric <annaric@users.noreply.github.com>
2025-03-24 13:13:26 +0000

    Prettified Code!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    env: {
-      node: true,
-      browser: true,
-      es2021: true,
-    },
-    parserOptions: {
-      sourceType: 'module',
-    },
-    overrides: [
-      {
-        files: ["*.ts", "*.tsx"],
-        parser: '@typescript-eslint/parser',
-        plugins: ['@typescript-eslint'],
-        parserOptions: {
-          project: 'tsconfig.json',
-        },
-        extends: [
-          'eslint:recommended',
-          'plugin:@typescript-eslint/recommended',
-          'plugin:@typescript-eslint/eslint-recommended',
-        ],
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      parserOptions: {
+        project: "tsconfig.json",
       },
-      {
-        files: ["*.js", "*.jsx"],
-        extends: ["eslint:recommended"],
-      },
-    ],
-  };
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+      ],
+    },
+    {
+      files: ["*.js", "*.jsx"],
+      extends: ["eslint:recommended"],
+    },
+  ],
+};

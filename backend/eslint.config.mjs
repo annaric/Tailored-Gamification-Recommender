a8f581f9912e5fecbf15fc7844c9f01608049ec4
annaric <annaric@users.noreply.github.com>
2025-03-26 21:49:09 +0000

    Prettified Code!

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,14 +6,21 @@ import tseslint from "typescript-eslint";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   tseslint.configs.recommended,
   {
     files: ["**/*.js"],
     rules: {
-      "@typescript-eslint/no-require-imports": "off"
-    }
-  }
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
   //muss noch geprüft werden, ob .ts Dateien jetzt auch geprüft werden
 ]);

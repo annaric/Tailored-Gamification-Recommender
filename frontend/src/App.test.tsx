3d834524d7ed011513c2ea3283ed3f88df2ee9ce
annaric <annaric@users.noreply.github.com>
2025-03-31 18:32:17 +0000

    Prettified Code!

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import {describe, expect, test} from '@jest/globals';
-import '@testing-library/jest-dom';
-
-
+import { describe, expect, test } from "@jest/globals";
+import "@testing-library/jest-dom";
 
 describe("App Component", () => {
   test("renders recommend button", () => {

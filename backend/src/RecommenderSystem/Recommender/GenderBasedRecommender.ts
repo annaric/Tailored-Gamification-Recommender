186794c093ef8453015a7104f3f1d916025c331a
annaric <annaric@users.noreply.github.com>
2025-04-08 12:09:31 +0000

    Prettified Code!

--- backend/src/RecommenderSystem/Recommender/GenderBasedRecommender.ts
+++ backend/src/RecommenderSystem/Recommender/GenderBasedRecommender.ts
@@ -67,15 +67,25 @@ class GenderBasedRecommender extends AbstractRecommender {
       femaleResultArray.push(element.female);
     });
     // Calculate the standard deviation of maleResultArray
-    const maleMean = maleResultArray.reduce((sum, value) => sum + value, 0) / maleResultArray.length;
+    const maleMean =
+      maleResultArray.reduce((sum, value) => sum + value, 0) /
+      maleResultArray.length;
     const maleStdDev = Math.sqrt(
-      maleResultArray.reduce((sum, value) => sum + Math.pow(value - maleMean, 2), 0) / maleResultArray.length
+      maleResultArray.reduce(
+        (sum, value) => sum + Math.pow(value - maleMean, 2),
+        0,
+      ) / maleResultArray.length,
     );
 
     // Calculate the standard deviation of femaleResultArray
-    const femaleMean = femaleResultArray.reduce((sum, value) => sum + value, 0) / femaleResultArray.length;
+    const femaleMean =
+      femaleResultArray.reduce((sum, value) => sum + value, 0) /
+      femaleResultArray.length;
     const femaleStdDev = Math.sqrt(
-      femaleResultArray.reduce((sum, value) => sum + Math.pow(value - femaleMean, 2), 0) / femaleResultArray.length
+      femaleResultArray.reduce(
+        (sum, value) => sum + Math.pow(value - femaleMean, 2),
+        0,
+      ) / femaleResultArray.length,
     );
 
     console.log("Male Standard Deviation:", maleStdDev);
@@ -94,10 +104,12 @@ class GenderBasedRecommender extends AbstractRecommender {
     // Normiere zwischen 0.5 und 1, wobei 1 der beste Wert ist.
     // Sinnvoll bei Review Paper, die nur positive "Korrelationen" zurückgeben.
     // Unterschied zu anderen Normalisierungen: der maxValue sorgt dafür, dass es mit den anderen möglichen Werten in Beziehung gesetzt wird
-    const maxValue = input.result.Incentive.male + input.result.Incentive.female;
+    const maxValue =
+      input.result.Incentive.male + input.result.Incentive.female;
     const resultElement = { male: 0, female: 0 };
     resultElement.male = 0.5 + (input.result.Incentive.male / maxValue) * 0.5;
-    resultElement.female = 0.5 + (input.result.Incentive.female / maxValue) * 0.5;
+    resultElement.female =
+      0.5 + (input.result.Incentive.female / maxValue) * 0.5;
     return resultElement;
   }
 
@@ -128,18 +140,17 @@ class GenderBasedRecommender extends AbstractRecommender {
   }
 
   normalizeCorrelationDataPaper(input: LiteratureElementObject) {
-     //Normieren zwischen 0 und 1 statt -1 und 1
+    //Normieren zwischen 0 und 1 statt -1 und 1
     const resultElement = { male: 0, female: 0 };
     resultElement.male = (input.result.Incentive.male + 1) / 2;
     resultElement.female = (input.result.Incentive.female + 1) / 2;
     return resultElement;
-
   }
 
   normalizeBinaryDataPaper(input: LiteratureElementObject) {
     const resultElement = { male: 0, female: 0 };
     resultElement.female = input.result.Incentive.female === 1 ? 0.75 : 0.25;
-    resultElement.male =  input.result.Incentive.male === 1 ? 0.75 : 0.25;
+    resultElement.male = input.result.Incentive.male === 1 ? 0.75 : 0.25;
     return resultElement;
   }
 }
--- backend/src/types/LiteratureTypeEnum.ts
+++ backend/src/types/LiteratureTypeEnum.ts
@@ -1,5 +1,5 @@
 export enum LiteratureResultTypeEnum {
-  PositiveNumber = "PositiveNumber",  // Review Paper, die nur positive "Korrelationen" zurückgeben. [0, maxValue]
+  PositiveNumber = "PositiveNumber", // Review Paper, die nur positive "Korrelationen" zurückgeben. [0, maxValue]
   Correlation = "Correlation", // Correlation Paper
   Binary = "Binary",
   Scale = "Scale", // Scale Paper, die den User nach Bewertung fragen zwischen mag ich garnicht und mag ich sehr.
